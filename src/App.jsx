4954bd9e8f1675b163105e065094723899641bfa
angelolustosa <alessandrofeitoza01@gmail.com>
2022-10-14 08:08:50 -0300

    inicial

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+import Header from "./components/header";
+import Banner from "./components/banner";
+
+function App() {
+  return (
+    <div>
+      <Header />
+      <Banner />
+    </div>
+  );
+}
+
+export default App;
