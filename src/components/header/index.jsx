3cd94c1013ff0f529ac9005ea1bd245333369d39
angelolustosa <alessandrofeitoza01@gmail.com>
2022-10-14 08:28:19 -0300

    Nunca vi mais lindo

--- src/components/header/index.jsx
+++ src/components/header/index.jsx
@@ -1,10 +1,12 @@
+import Carrinho from './carrinho'
+
 export default function Header() {
     return (<>
         <div>logo</div>
         <div>buscar</div>
         <div>cadastre-se</div>
         <div>entrar</div>
-        <div>carrinho</div>
+        <Carrinho />
         <div>nav</div>
     </>)
 }
---
+++ src/components/produtos/produto/index.jsx
@@ -0,0 +1,5 @@
+export default function Produto(props) {
+    return (<div style={{ border: '1px solid black' }}>
+        {props.titulo}
+    </div>)
+}
